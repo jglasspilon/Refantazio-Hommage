b87c8db26446d9229cbc467698815fb4fee5553e
Julian_Glass-Pilon <julian@astucemedia.com>
2026-01-22 16:43:54 -0500

    Completed page selection list cycling animation and page index display for main menu page

---
+++ Assets/Code/Scripts/UI/Menu/MainMenuPage/PageIndexDisplay.cs
@@ -0,0 +1,97 @@
+using TMPro;
+using UnityEngine;
+
+public class PageIndexDisplay : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI m_pageIndexText;
+
+    [SerializeField]
+    private PageSelecterList m_pageSelecterList;
+
+    [SerializeField]
+    private float m_nudgeDistance;
+
+    [SerializeField]
+    private float m_nudgeSpeed;
+
+    private Vector3 m_startPosition;
+    private Vector3 m_nudgePosition;
+    private bool m_isNudged;
+
+    private void Awake()
+    {
+        m_startPosition = transform.localPosition;
+        m_nudgePosition = new Vector3(m_startPosition.x, m_startPosition.y - m_nudgeDistance, m_startPosition.z);
+    }
+
+    private void OnEnable()
+    {
+        m_pageSelecterList.OnIndexChanged += UpdateText;
+        UpdateText(m_pageSelecterList.CurrentPageIndex);
+    }
+
+    private void OnDisable()
+    {
+        m_pageSelecterList.OnIndexChanged -= UpdateText;
+    }
+
+    private void Update()
+    {
+        if (transform.localPosition.y < m_startPosition.y)
+        {
+            float step = m_nudgeSpeed * Time.deltaTime;
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y + step, transform.localPosition.z);
+        }
+        else if (transform.localPosition != m_startPosition)
+        {
+            transform.localPosition = m_startPosition;
+            m_isNudged = false;
+        }
+    }
+
+    private void UpdateText(int pageIndex)
+    {
+        SetPageText(pageIndex);
+
+        if(!m_isNudged)
+            Nudge();
+    }
+
+    private void Nudge()
+    {
+        m_isNudged = true;
+        transform.localPosition = m_nudgePosition;
+    }
+
+    private void SetPageText(int pageIndex)
+    {
+        string result = pageIndex.ToString("0");
+        string richTextFix = "";
+        float xScale = 1.0f;
+
+        //Manually fix the text size, voffset and xScale based on number since the font sheet used does not have optimal numeral glyph sizing
+        switch (pageIndex)
+        {
+            case 1:
+                xScale = 1.4f;
+                break;
+            case 2:
+                break;
+            case 3:
+            case 4:
+            case 5:
+            case 7:
+            case 9:
+                richTextFix = "<size=85%><voffset=80>";
+                break;
+            case 6:
+            case 8:
+                richTextFix = "<size=85%><voffset=20>";
+                break;
+        }
+
+        m_pageIndexText.transform.localScale = new Vector3(xScale, 1, 1);
+        m_pageIndexText.text = $"{richTextFix}{result}";
+    }
+}
--- Assets/Code/Scripts/UI/Menu/MainMenuPage/PageSelecterList.cs
+++ Assets/Code/Scripts/UI/Menu/MainMenuPage/PageSelecterList.cs
@@ -13,7 +13,7 @@ public class PageSelecterList : MonoBehaviour
     private int m_startPageIndex;
 
     [SerializeField]
-    private TextMeshProUGUI m_pageIndexText;
+    private Animator m_anim;
 
     private int m_currentPageIndex;
     private Dictionary<int, PageSelecter> m_pageSelectors;
@@ -25,11 +25,12 @@ public class PageSelecterList : MonoBehaviour
     private void Awake()
     {
         m_pageSelectors = GetComponentsInChildren<PageSelecter>().ToDictionary(x => (int)x.TargetPage, x => x);
+        m_currentPageIndex = m_startPageIndex;
     }
 
-    private void Start()
+    private void OnEnable()
     {
-        SetPageIndex(m_startPageIndex);
+        SetPageIndex(m_currentPageIndex);
     }
 
     public void CycleUp()
@@ -54,43 +55,13 @@ public class PageSelecterList : MonoBehaviour
             newIndex = MIN_INDEX;
 
         m_currentPageIndex = newIndex;
-        SetPageText(m_currentPageIndex);
+        
+        m_anim.SetInteger("PageIndex", m_currentPageIndex);
         OnIndexChanged?.Invoke(m_currentPageIndex);
     }
 
     public void ResetToDefault()
     {
         SetPageIndex(m_startPageIndex);
-    }
-
-    private void SetPageText(int pageIndex)
-    {
-        string result = pageIndex.ToString("0");
-        string richTextFix = "";
-        float xScale = 1.0f;
-
-        //Manually fix the text size, voffset and xScale based on number since the font sheet used does not have optimal numeral glyph sizing
-        switch(pageIndex)
-        {
-            case 1:
-                xScale = 1.4f;
-                break;
-            case 2:
-                break;
-            case 3:
-            case 4:
-            case 5:
-            case 7:
-            case 9:
-                richTextFix = "<size=85%><voffset=80>";
-                break;
-            case 6:
-            case 8:
-                richTextFix = "<size=85%><voffset=20>";
-                break;
-        }
-
-        m_pageIndexText.transform.localScale = new Vector3(xScale, 1, 1);
-        m_pageIndexText.text = $"{richTextFix}{result}";
     }
 }
