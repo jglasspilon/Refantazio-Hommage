ffb3aebc66bdf8e13cad8ebb0c8975dfef308e6b
Julian_Glass-Pilon <julian@astucemedia.com>
2026-01-29 16:39:22 -0500

    Created Item menu page and all of its animations

---
+++ Assets/Code/Scripts/UI/Menu/ItemsPage/ItemsMenuPage.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class ItemsMenuPage : MenuPage
+{
+    public override void Confirm()
+    {
+        
+    }
+
+    public override void CycleDown()
+    {
+        
+    }
+
+    public override void CycleUp()
+    {
+        
+    }
+
+    public override void ResetPage()
+    {
+        
+    }
+}
